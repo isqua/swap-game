88d54120127f33f6f8ae82a55037e9e6117a02a5
isqua <isqua@isqua.ru>
2024-08-09 01:34:32 +0300

    feat: introduce initial game state

---
+++ src/lib/Board.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Cell from './Cell.svelte';
+	import Chip from './Chip.svelte';
+
+	let state = [1, 1, 1, 1, 0, -1, -1, -1, -1];
+</script>
+
+<ul>
+	{#each state as value, index}
+		<Cell>
+			<Chip {value} onClick={() => console.log({ value, index })} />
+		</Cell>
+	{/each}
+</ul>
---
+++ src/lib/Chip.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let value;
+	export let onClick;
+</script>
+
+<button type="button" class="cell" on:click={onClick}>
+	{value}
+</button>
