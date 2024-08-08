1eb170fcf44c9b0d030cc6788e6d4101c243fc22
isqua <isqua@isqua.ru>
2024-08-09 02:45:24 +0300

    feat: connect gameplay to the ui

---
+++ src/game/index.ts
@@ -0,0 +1,1 @@
+export { restart, move } from './game';
--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
+	import { move, restart } from '../game';
+
 	import Cell from './Cell.svelte';
 	import Chip from './Chip.svelte';
 
-	let state = [1, 1, 1, 1, 0, -1, -1, -1, -1];
+	let state = restart();
+
+	function moveChip(index: number) {
+		state = move(state, index);
+	}
 </script>
 
 <ul>
 	{#each state as value, index}
 		<Cell>
-			<Chip {value} onClick={() => console.log({ value, index })} />
+			<Chip {value} onClick={() => moveChip(index)} />
 		</Cell>
 	{/each}
 </ul>
