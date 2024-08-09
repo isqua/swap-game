1df293319b443541dca86c0902b4f1b901ceddf3
isqua <isqua@isqua.ru>
2024-08-09 13:58:35 +0300

    feat: introduce history state

---
+++ src/game/history.test.ts
@@ -0,0 +1,61 @@
+import { describe, it, expect } from 'vitest';
+import { restart, add, undo, redo, getEvents, canUndo, canRedo } from './history';
+
+describe('History', () => {
+	it('should restart the history', () => {
+		const state = restart<string>();
+
+		expect(getEvents(state)).toEqual([]);
+		expect(canUndo(state)).toEqual(false);
+		expect(canRedo(state)).toEqual(false);
+	});
+
+	it('should add events to the history', () => {
+		let state = restart<string>();
+		state = add(state, 'event1');
+		state = add(state, 'event2');
+		state = add(state, 'event3');
+
+		expect(getEvents(state)).toEqual(['event1', 'event2', 'event3']);
+		expect(canUndo(state)).toEqual(true);
+		expect(canRedo(state)).toEqual(false);
+	});
+
+	it('should undo an event', () => {
+		let state = restart<string>();
+		state = add(state, 'event1');
+		state = undo(state);
+
+		expect(getEvents(state)).toEqual([]);
+		expect(canUndo(state)).toEqual(false);
+		expect(canRedo(state)).toEqual(true);
+	});
+
+	it('should redo an event', () => {
+		let state = restart<string>();
+		state = add(state, 'event1');
+		state = add(state, 'event2');
+		state = undo(state);
+		state = redo(state);
+
+		expect(getEvents(state)).toEqual(['event1', 'event2']);
+	});
+
+	it('should not break if there is nothing to undo', () => {
+		let state = restart<string>();
+		state = undo(state);
+		state = undo(state);
+		state = add(state, 'event1');
+
+		expect(getEvents(state)).toEqual(['event1']);
+	});
+
+	it('should not break if there is nothing to redo', () => {
+		let state = restart<string>();
+		state = add(state, 'event1');
+		state = redo(state);
+		state = add(state, 'event2');
+
+		expect(getEvents(state)).toEqual(['event1', 'event2']);
+	});
+});
---
+++ src/game/history.ts
@@ -0,0 +1,32 @@
+export type HistoryState<T> = {
+	events: Array<T>;
+	current: number;
+};
+
+export const restart = <T>(): HistoryState<T> => ({
+	events: [],
+	current: -1
+});
+
+export const add = <T>(state: HistoryState<T>, event: T): HistoryState<T> => ({
+	events: [...state.events, event],
+	current: state.current + 1
+});
+
+export const undo = <T>(state: HistoryState<T>): HistoryState<T> => ({
+	events: state.events,
+	current: Math.max(-1, state.current - 1)
+});
+
+export const redo = <T>(state: HistoryState<T>): HistoryState<T> => ({
+	events: state.events,
+	current: Math.min(state.events.length - 1, state.current + 1)
+});
+
+export const getEvents = <T>(state: HistoryState<T>): Array<T> =>
+	state.events.slice(0, state.current + 1);
+
+export const canUndo = <T>(state: HistoryState<T>): boolean => state.current >= 0;
+
+export const canRedo = <T>(state: HistoryState<T>): boolean =>
+	state.current < state.events.length - 1;
