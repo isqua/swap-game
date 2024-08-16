fbe658b07f055bb5c0b5983589a048d32e1285f7
isqua <isqua@isqua.ru>
2024-08-16 03:22:48 +0300

    feat: add fun fonts for win and loose text

--- src/lib/Status.svelte
+++ src/lib/Status.svelte
@@ -4,11 +4,20 @@
 	export let state: GameState;
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <div class="status">
 	{#if state === 'win'}
-		<p>You win!</p>
+		<p class="finish">You win!</p>
 	{:else if state === 'loose'}
-		<p>You loose!</p>
+		<p class="finish">You loose!</p>
 	{:else}
 		<p>
 			Swap the balls! Each ball can move to an empty cell if it is adjacent. It can also jump over 1
@@ -24,5 +33,24 @@
 		margin: auto;
 		padding: 2em;
 		text-align: center;
+	}
+
+	.finish {
+		max-width: none;
+		font-family: 'Luckiest Guy';
+		letter-spacing: 0.05em;
+		color: #fff;
+		font-size: 15vmin;
+		text-shadow:
+			0 -0.04em 0 #212121,
+			0.04em -0.04em 0 #212121,
+			0.04em 0 0 #212121,
+			0.04em 0.04em 0 #212121,
+			0 0.04em 0 #212121,
+			-0.04em 0.04em 0 #212121,
+			-0.04em 0 0 #212121,
+			-0.04em -0.04em 0 #212121,
+			0em 0.07em 0.03em rgba(0, 0, 0, 0.3),
+			0em 0.15em 0.2em rgba(0, 0, 0, 0.25);
 	}
 </style>
